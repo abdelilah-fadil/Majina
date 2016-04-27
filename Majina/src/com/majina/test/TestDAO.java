a0ea64b94528cd4df45f36dda96f86603365986d
FADIL Abdelilah <fadil.abdelilah@gmail.com>
2016-04-28 00:07:09 +0100

    hibernate conf

--- Majina/src/com/majina/test/TestDAO.java
+++ Majina/src/com/majina/test/TestDAO.java
@@ -1,7 +1,5 @@
 package com.majina.test;
 
-import java.util.Date;
-
 import com.majina.dao.AdminDAOImpl;
 import com.majina.model.Admin;
 
@@ -10,10 +8,7 @@ public class TestDAO {
 	public static void main(String[] args) {
 		AdminDAOImpl adminDAOImpl = new AdminDAOImpl();
 		adminDAOImpl.createAdmin(new Admin());
-		
-		Date date = new Date();
-		System.out.println(date);
-		
+		System.out.println("test");
 
 	}
 
